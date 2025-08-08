78ce3f5e9940a18659ba9d54e276980e66dc43a4
Ricardo Oliveira Leandro <48275184835@>
2025-08-08 11:04:31 -0300

    Meu segundo commit

---
+++ back/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import AutoresView
+
+urlpatterns = [
+    path('autores', AutoresView.as_view()),
+    
+]
--- back/api/views.py
+++ back/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import ListCreateAPIView
+from .models import Autor
+from .serializers import AutorSerializer
+
+class AutoresView(ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+    
