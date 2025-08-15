3b086558e997c7ef677bcd8a814a21671b384ea2
Ricardo Oliveira Leandro <48275184835@>
2025-08-15 11:01:57 -0300

    moldando com json

--- back/api/urls.py
+++ back/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AutoresView
+from .views import AutoresView, listar_autores
 
 urlpatterns = [
     path('autores', AutoresView.as_view()),
+    path("authors" , listar_autores)
     
 ]
