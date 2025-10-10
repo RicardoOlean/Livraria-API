6b46e792a99fc9547708b78d428a93d6ed764b29
Ricardo Oliveira Leandro <48275184835@>
2025-10-10 07:46:56 -0300

    Adicionando o back

---
+++ api/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from .models import Autor, Editora, Livro
+
+# === ADICIONE: imports para o cadastro ===
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.validators import UniqueValidator
+
+class AutorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = '__all__'
+
+class EditoraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = '__all__'
+
+
+class LivroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+        
+        
+        
+
+# === ADICIONE: serializer de registro de usuário ===
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all(), message="Usuário já existe.")]
+    )
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password],
+        style={'input_type': 'password'}
+    )
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+
+    def create(self, validated_data):
+        return User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
